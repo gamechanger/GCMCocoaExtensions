a288e0b3c53b4fed5321cd13fe1d67b8d744b014
Ami Kumar <amikumar@Amis-MacBook-Pro.local>
2015-08-04 13:04:56 -0400

    Desupport ios7

--- GCMCocoaExtensions/GCMCocoaExtensions/NSString+GameChangerMedia.m
+++ GCMCocoaExtensions/GCMCocoaExtensions/NSString+GameChangerMedia.m
@@ -36,24 +36,16 @@
 }
 
 - (CGFloat)heightForStringUsingWidth:(CGFloat)width andFont:(UIFont *)font {
+  NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
+  paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
 
-    if ( IOS7_OR_GREATER) {
-        NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
-        paragraphStyle.lineBreakMode = NSLineBreakByWordWrapping;
+  CGRect rect = [self boundingRectWithSize:CGSizeMake(width, 9999.f)
+                                   options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingTruncatesLastVisibleLine
+                                attributes:@{NSFontAttributeName : font,
+                                             NSParagraphStyleAttributeName : paragraphStyle}
+                                   context:nil];
 
-        CGRect rect = [self boundingRectWithSize:CGSizeMake(width, 9999.f)
-                                         options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingTruncatesLastVisibleLine
-                                      attributes:@{NSFontAttributeName : font,
-                                                   NSParagraphStyleAttributeName : paragraphStyle}
-                                         context:nil];
-
-        return ceil(rect.size.height);
-    } else {
-        CGSize size = [self sizeWithFont:font
-                       constrainedToSize:CGSizeMake(width, 9999.f)
-                           lineBreakMode:NSLineBreakByWordWrapping];
-        return ceil(size.height);
-    }
+  return ceil(rect.size.height);
 }
 
 @end
